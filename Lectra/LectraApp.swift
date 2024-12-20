22d4add539d3387a1846c47d04162283b70be136
KyleAnthonyHay <haykyle917@gmail.com>
2024-12-19 23:53:44 -0500

    Initial Commit

---
+++ Lectra/LectraApp.swift
@@ -0,0 +1,17 @@
+//
+//  LectraApp.swift
+//  Lectra
+//
+//  Created by Kyle-Anthony Hay on 12/19/24.
+//
+
+import SwiftUI
+
+@main
+struct LectraApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
